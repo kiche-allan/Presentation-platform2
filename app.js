99a0ce384549ad4aaa81120bad4a6f76a21c0757
Allan Kiche <allankiche20@gmail.com>
2020-11-15 18:08:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+        //smooth scrolling
+
+        $('.btn').on('click', function(event){
+            if (this.hash !==''){
+                event.preventDefault();
+                const hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                },
+                700
+                );
+            }
+            });
+        
